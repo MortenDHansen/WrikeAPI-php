bdb58b9a1e33477b6c7151559c715ac92fd509a7
morten <morten@Lenovo-PC>
2015-05-30 17:47:46 +0200

    Initial Commit

---
+++ api_handler.php
@@ -0,0 +1,129 @@
+<?php
+// Check this constant. Just to see that we are not being directly requested...
+if(!defined('angus')) {
+    die('Brothers Malcolm, Angus, and George Young were born in Glasgow, Scotland, and moved to Sydney with most of their family in 1963.');
+}
+
+class Wrikeapi
+{
+    // Set the the auth credentials obtained from wrike
+    private $client_id = 'ADnZquor';
+    private $client_secret = 'BfWIsdDyJ3s4AEX1LQqOmYh86AiyzDwFqe3UhzhfQgd9ZqmPcYpxgpaR5xqXqtpm';
+    private $code = 'MUZzoGANkuFaO2J00lWBczB78zxc9fbIXR1TMO1bwlvMq4qpvCMWRB3lUJY0ShpS-N'; // when you agree to give acces through the api, you get this code.
+    private $refresh_token = 'joJ8KDnQiXkLt2VEY83Ch7mF5s9Jb6O5xjGglMzhA2Hy4xkKc5Hm3wHjtmoMRYkz-A-N';
+    private $api_url = 'https://www.wrike.com/api/v3';
+    // recieve the token
+    
+    /*
+     * Run this method to get the refresh_token. It should only be run once as it will reset the authentification and requre a fresh auth-code.
+     */
+    function token_init()
+    {
+        $url = "https://www.wrike.com/oauth2/token";
+        $fields = array(
+            'client_id'=>$this->client_id, 
+            'client_secret'=>$this->client_secret, 
+            'grant_type'=>'authorization_code',
+            'code'=>$this->code
+            );
+        $token = $this->curl_post($url, $fields);
+        $token = json_decode($token);
+        echo "note: This should only be run ONCE for your application. From here on, only REFRESH - asking for a new one will require you to update the authorization_code";
+        return $token; // be sure to keep the refresh token handy (comes with the json).
+    }
+
+    // call this to refresh
+    private function _refresh_token()
+    {
+        $url = "https://www.wrike.com/oauth2/token";
+        $fields = array(
+            'client_id'=>$this->client_id, 
+            'client_secret'=>$this->client_secret, 
+            'grant_type'=>'refresh_token', 
+            'refresh_token'=>$this->refresh_token
+            );
+        $token = curl_post($url, $fields);
+        return $token;
+    }
+    
+    function api_version($token)
+    {
+        $url = 'https://www.wrike.com/api/v3/version';
+        $result = $this->_fetch_result($token,$url);
+        return $result;
+    }
+    
+    /*
+     * Information about the accounts this user can access
+     * Plese note the ID returned
+     */
+    function get_accounts($token)
+    {
+        $url = $this->api_url . '/accounts';
+        $result = $this->curl_get($url, $token);
+        return $result;
+    }
+    function get_folders($token, $accountid = '') 
+    {
+        if($accountid == ''){
+            echo "If you wont provide me with the needed input, i wont provide you with the output...";exit;
+        }
+        $url = $this->api_url . '/accounts/' . $accountid . '/folders';
+        $result = $this->_fetch_result($token,$url);
+        return $result;
+    }
+    function get_tasks($token, $folder, $params = '')
+    {
+        $url = $this->api_url . '/folders/'.$folder.'/tasks';
+        $result = $this->_fetch_result($token,$url);
+        return $result;
+    }
+    
+    /*
+     * Get the job done - or provide you with a reaon why i was not done
+     */
+    private function _fetch_result($token = '',$url = '') 
+    {
+        if($token == '' || $url == '') {
+            echo "FUCK ME! - Something was not right - take a look at this:";
+            echo "... you are not giving me enough to work with. I am missing params!";
+            exit('SUICIDE');
+        }
+        
+        $result = $this->curl_get($url, $token);
+        $evaluate_result = json_decode($result);
+        if(isset($evaluate_result->error)){
+            echo "FUCK ME! - Something was not right - take a look at this:";
+            var_dump($evaluate_result);exit('SUICIDE');
+        } else { 
+            return $result;
+        }
+    }
+    function curl_post($url,$fields)
+    {
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, $url);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
+        curl_setopt($ch, CURLOPT_POST, 1);
+        curl_setopt($ch, CURLOPT_POSTFIELDS, $fields);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0); // On dev server only!
+        $result = curl_exec($ch);
+        return $result;
+    }
+    function curl_get($url,$token)
+    {
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, $url);
+        curl_setopt($ch, CURLOPT_HEADER, false);
+        curl_setopt($ch, CURLOPT_HTTPHEADER, array('Authorization: bearer ' . $token ));
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
+        curl_setopt($ch, CURLOPT_POST, 0);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0); // On dev server only!
+        $result = curl_exec($ch);
+        return $result;
+    }
+    
+}
+
---
+++ index.php
@@ -0,0 +1,29 @@
+<?php
+    define('angus',TRUE); // setting a constant in order to prevent direct access. Low practical - i know...
+    require 'api_handler.php';
+    $api = new wrikeapi();
+    // Look to the api_handler.php - method token_init can help you make the access token.
+    // you should be able to recieve the token.
+    $access_token = 'oHjAySOkzBJuLkLcvBWEIMonDzYITKlTTSeXNZZuIy7sohmn0NdzzOUhq0AnZDgR-N-N';
+?>
+<!DOCTYPE html>
+<!--
+ Morten D. Hansen
+ 2015 mdh@skyadsmedia.com
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Wrike API</title>
+    </head>
+    <body>
+        <p>running</p>
+        <?php
+            // check that the API is running - this gets the version
+            $version = $api->api_version($access_token);
+            $version = json_decode($version);
+            echo "<p>The api version you are requesting is: " . $version->data[0]->major . "." . $version->data[0]->minor . "<p>";
+            echo "That went well!";
+        ?>
+    </body>
+</html>
